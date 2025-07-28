1f5b2485ab49f2a959a00248ab4d5aa802f3c76b
Crazdrh <hayden.glahe@gmail.com>
2025-07-28 18:52:19 -0400

    Update alpacaapi.py

--- alpacaapi.py
+++ alpacaapi.py
@@ -10,7 +10,28 @@ API_KEY = ""
 API_SECRET = ""
 client = StockHistoricalDataClient(API_KEY, API_SECRET)
 
-SYMBOLS = ["AAPL", "GOOGL", "NVDA"]  # <-- Edit this list for your stocks
+SYMBOLS = sp500_tickers = [
+    "AAPL", "MSFT", "AMZN", "NVDA", "GOOGL", "GOOG", "META", "BRK.B", "UNH", "LLY", "JPM", "V", "XOM", "TSLA", "AVGO",
+    "PG", "MA", "JNJ", "COST", "MRK", "HD", "ADBE", "ABBV", "CRM", "CVX", "PEP", "TMO", "NFLX", "KO", "ACN", "LIN",
+    "AMD", "WMT", "ORCL", "MCD", "ABT", "DHR", "CSCO", "QCOM", "DIS", "VZ", "WFC", "INTU", "TXN", "HON", "NEE", "LOW",
+    "PM", "MS", "AMGN", "AMAT", "CAT", "UNP", "IBM", "BA", "GS", "RTX", "SPGI", "GE", "ISRG", "MDT", "PLD", "LMT",
+    "ADP", "T", "CVS", "MU", "NOW", "BLK", "C", "SYK", "ELV", "DE", "SCHW", "EL", "GILD", "REGN", "BDX", "ZTS", "TGT",
+    "VRTX", "MO", "CB", "SO", "USB", "MMC", "CI", "CL", "DUK", "PNC", "BKNG", "AON", "FISV", "SLB", "ADSK", "ETN",
+    "FDX", "ITW", "TJX", "GM", "PGR", "NSC", "EQIX", "EOG", "LRCX", "MPC", "AEP", "EW", "SRE", "NOC", "SHW", "APD",
+    "CSX", "ROP", "GD", "FIS", "IDXX", "WMB", "EMR", "STZ", "PSA", "EXC", "D", "AIG", "FCX", "PAYX", "KMB", "AZO",
+    "OXY", "TRV", "HUM", "AFL", "PH", "ORLY", "MCK", "PXD", "PSX", "CNC", "MSCI", "COF", "ECL", "HLT", "MAR", "CDNS",
+    "BAX", "HCA", "CTSH", "ROP", "SPG", "KMI", "O", "ALL", "DLR", "DOV", "A", "BKR", "CNP", "AVB", "BBY", "BXP", "CDW",
+    "CF", "CHD", "CINF", "CMA", "CMS", "CNP", "COO", "CPB", "CRL", "CTAS", "DG", "DHI", "DLTR", "DRI", "EIX", "ESS",
+    "F", "FANG", "FAST", "FMC", "FRC", "FTNT", "GL", "GLW", "GRMN", "GWW", "HAS", "HIG", "HOLX", "HST", "HSY", "IFF",
+    "ILMN", "INCY", "IP", "IPG", "IR", "IRM", "JBHT", "JCI", "JKHY", "JNPR", "K", "KEY", "KEYS", "KIM", "KMX", "KR",
+    "LEG", "LEN", "LH", "LKQ", "LNT", "LUV", "LW", "MAS", "MKC", "MKTX", "MLM", "MNST", "MOS", "MPWR", "MTB", "MTCH",
+    "NDAQ", "NDSN", "NEM", "NI", "NUE", "NVR", "NWL", "NWS", "NWSA", "ODFL", "OGN", "OKE", "OMC", "ON", "OTIS", "PARA",
+    "PAYC", "PBCT", "PCAR", "PEAK", "PKG", "PODD", "POOL", "PPL", "PRGO", "PRU", "PTC", "PVH", "QRVO", "RCL", "RJF",
+    "RMD", "ROL", "RSG", "SBAC", "SBNY", "SBUX", "SEDG", "SEE", "SJM", "SMG", "SNA", "SNPS", "SNY", "SPSC", "STE",
+    "SWK", "SWKS", "SYY", "TAP", "TDY", "TECH", "TER", "TFC", "TPR", "TROW", "TSCO", "TTWO", "UAL", "UDR", "UHS", "ULTA",
+    "URI", "VFC", "VICI", "VLO", "VMC", "VTR", "VTRS", "VZ", "WAB", "WAT", "WDC", "WELL", "WLTW", "WRB", "WY", "WYN",
+    "XEL", "XYL", "YUM", "ZBH", "ZBRA"
+]# <-- Edit this list for your stocks
 start_date = date(2020, 7, 18)
 end_date   = date(2025, 6, 18)
 chunk_days = 15  # 15 days at a time
